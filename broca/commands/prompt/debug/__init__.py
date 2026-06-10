abad566af9679a586efa41db2a3416a2df2c8688
Rossi <1142595193@qq.com>
2026-06-10 10:00:30 +0800

    feature:增加新命令

---
+++ broca/commands/prompt/debug/__init__.py
@@ -0,0 +1,6 @@
+from broca.commands.base import PromptCommand
+
+
+class DebugCommand(PromptCommand):
+    """Debug a bug with systematic methodology."""
+    pass
---
+++ broca/commands/prompt/review_code/__init__.py
@@ -0,0 +1,6 @@
+from broca.commands.base import PromptCommand
+
+
+class ReviewCodeCommand(PromptCommand):
+    """Review code quality with systematic methodology."""
+    pass
